e5d3b265738c733186d5b3f10d413a0c355c47a4
Alejandro Larumbe <larumbemar@gmail.com>
2021-01-18 20:49:40 +1300

    sunday

--- react-app/src/components/resources/ViewResources.js
+++ react-app/src/components/resources/ViewResources.js
@@ -13,6 +13,9 @@ import IconButton from '@material-ui/core/IconButton';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,6 +29,12 @@ const useStyles = makeStyles(theme => ({
 export default function ViewResources({ resources }) {
   const [type, setType] = useState(2);
   const classes = useStyles();
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
 
   const handleChange = (event, newValue) => {
@@ -34,46 +43,55 @@ export default function ViewResources({ resources }) {
 
 
   return (
-    <Paper className={classes.root} square>
-      <Tabs
-        value={type}
-        indicatorColor="primary"
-        textColor="primary"
-        onChange={handleChange}
-        aria-label="disabled tabs example"
-      >
-        <Tab label="Audio" value={2} />
-        <Tab label="Video" value={3} />
-        <Tab label="PDF" value={1} />
-      </Tabs>
-
-
-      {/* { resources ? */}
-      <List className={classes.root}>
-        {resources ? resources.map((resource, i) => {
-          if (resource.resourceTypeId === type) {
-            return (
-              <ListItem key={resource.id} role={undefined} button >
-                <ListItemText id={resource.id} primary={resource.title} />
-                <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="comments">
-                    {type === 1 ?
-                      <MenuBookIcon />
-                      :
-                      <PlayArrowIcon />
-                    }
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            );
+    <>
+      <Paper className={classes.root} square>
+        <Tabs
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleChange}
+          aria-label="disabled tabs example"
+        >
+          <Tab label="Audio" value={2} />
+          <Tab label="Video" value={3} />
+          <Tab label="PDF" value={1} />
+        </Tabs>
+        <List className={classes.root}>
+          {resources ? resources.map((resource, i) => {
+            if (resource.resourceTypeId === type) {
+              return (
+                <ListItem key={resource.id} role={undefined} button >
+                  <ListItemText id={resource.id} primary={resource.title} />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="comments">
+                      {type === 1 ?
+                        <MenuBookIcon />
+                        :
+                        <PlayArrowIcon />
+                      }
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            }
+          }) :
+            null
           }
-        }) :
-          null
-        }
-      </List>
-      {/* } : null */}
+        </List>
+      </Paper>
+       <div>
+        <Document
+          onLoadError={console.error}
+          file={{ url: `https://cors-anywhere.herokuapp.com/https://ace-management.s3.us-east-2.amazonaws.com/resources/dlscrib.com-pdf-galamian-contemporary-violin-technique-scales-dl_89baf79ec922d8863966d3428c8ee2c3.pdf_76401ee4-9bac-4430-b701-7b53fffecb6a` }}
+          // file={{ url: '/ace-management.s3.us-east-2.amazonaws.com/resources/dlscrib.com-pdf-galamian-contemporary-violin-technique-scales-dl_89baf79ec922d8863966d3428c8ee2c3.pdf_76401ee4-9bac-4430-b701-7b53fffecb6a'}}
 
-
-    </Paper>
+          // file="https://ace-management.s3.us-east-2.amazonaws.com/resources/dlscrib.com-pdf-galamian-contemporary-violin-technique-scales-dl_89baf79ec922d8863966d3428c8ee2c3.pdf_76401ee4-9bac-4430-b701-7b53fffecb6a"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    </>
   );
 }
