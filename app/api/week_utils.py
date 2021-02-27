df83d50289e4c9d7f3c05fe1a6fb07f085aa5398
Alejandro Larumbe <larumbemar@gmail.com>
2021-02-28 12:09:36 +1300

    clean uo

--- app/api/week_utils.py
+++ app/api/week_utils.py
@@ -3,12 +3,6 @@ import calendar
 from isoweek import Week
 
 
-
-# day = 1
-# month = 12
-# year = 2020
-
-
 def week_dates(year, month, day):
   date = datetime.date(year, month, day)
   iso_week = datetime.date(year, month, day).isocalendar()[1]
